7035d4668fd3b2cc5c977a812637c3359b707b9d
Kioko <kiooooooko@gmail.com>
2025-02-28 14:32:41 +0900

    回滚到修复前

--- Diary/View/CreateDiary/CreateDiaryView.swift
+++ Diary/View/CreateDiary/CreateDiaryView.swift
@@ -1,4 +1,4 @@
-//
+
 //  CreateDiaryView.swift
 //  Diary
 //
@@ -14,6 +14,7 @@ struct CreateDiaryView: View {
     @EnvironmentObject private var weatherData: WeatherData
     @EnvironmentObject private var bannerState: BannerState
     @EnvironmentObject private var textOptions: TextOptions
+    @EnvironmentObject private var sceneDelegate: DiaryAppSceneDelegate
 
     @StateObject private var diaryDataStore: DiaryDataStore = DiaryDataStore()
 
@@ -54,6 +55,7 @@ struct CreateDiaryView: View {
                 isTextEditorPresented = false
             }
         })
+        .environmentObject(sceneDelegate)
     }
 }
 
